42a662896d20047b01573b82f8da0b39c9823f85
orla-ske <kodjoorlando@yahoo.co.uk>
2022-10-23 19:24:40 +0000

    update to dashboard html amd css

--- midsemProjUltrLcd/midsemProjUltrLcd.ino
+++ midsemProjUltrLcd/midsemProjUltrLcd.ino
@@ -3,7 +3,7 @@
 //Library version:1.1
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-#include  <Ultrasonic.h>
+
 
 LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 Ultrasonic ultrasonic(14,27); // (Trig PIN,Echo PIN)
