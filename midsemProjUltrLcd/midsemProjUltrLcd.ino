97e970e93f79b23b66c790eaea3bc8db612876bd
orla-ske <kodjoorlando@yahoo.co.uk>
2022-10-25 02:08:25 +0000

    new addition

--- midsemProjUltrLcd/midsemProjUltrLcd.ino
+++ midsemProjUltrLcd/midsemProjUltrLcd.ino
@@ -3,6 +3,7 @@
 //Library version:1.1
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#define LEDRED (4)
 
 
 LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
@@ -16,14 +17,23 @@ void setup()
   lcd.backlight();
   lcd.setCursor(0,0);
   lcd.print("Testing...");
+  pinMode(LEDRED, OUTPUT);
  
 }
 
 void loop()
 {
   lcd.clear();
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0,0);   //Set cursor to character 2 on line 0
+  lcd.print("Water Level(cm)");
+  lcd.setCursor(4, 1);
   lcd.print(ultrasonic.Ranging(CM)); // CM or INC
   lcd.print("cm");
   delay(100);
+
+  if(ultrasonic.Ranging(CM)=< 15){
+    digitalWrite(LEDRED, HIGH); 
+    delay(1000); 
+    digitalWrite(LEDRED,LOW);
+  }
 }
