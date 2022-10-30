003b04b7631007649c010ff9a1dd723d1bfbe7d9
orla-ske <kodjoorlando@yahoo.co.uk>
2022-10-30 16:18:01 +0000

    bug fixes for liveData.php

--- php/liveData.php
+++ php/liveData.php
@@ -3,7 +3,8 @@ require "db.php";
 
 $sql = "select * from waterLevel";
 $result = mysqli_query($con,$sql);
-$row=mysqli_fetch_object($sql);
+$q = mysqli_query($con,"select * from waterLevel");
+$row=mysqli_fetch_object($q);
 if($row){
   while($row=$result->fetch_assoc()){
     echo "<tr><td>".$row["ID"]."</td><td>".$row["Date"]."</td></tr>"."</td><td>".$row["tankID"]."</td></tr>"."</td><td>".$row["waterLevel"]."</td></tr>";
